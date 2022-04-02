8cfbe096964255b48223dd81d6d060587aef7e83
Sam Wallace <sam@sammymayhem.com>
2022-04-02 13:39:06 -0700

    Added functionality to make starter text and button dissapear on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 var timerEl = document.getElementById("timer");
 var startBtn = document.getElementById("start-btn");
 
+
+const Questions = [{
+    id: 0,
+    q: "What is the answer to the first question?",
+    a: [{text: "This is not the answer", isCorrect: false},
+        {text: "This is the answer", isCorrect: true},
+        {text: "This is not the answer", isCorrect: false},
+        {text: "This is not the answer", isCorrect: false}
+]
+}]
+
 startBtn.addEventListener("click", function () {
     seconds = 5;
+
+    var starterText = document.getElementById("starter-text");
+        if (starterText.style.display === "none") {
+            starterText.style.display = "block";
+        } else {
+            starterText.style.display = "none"
+        }
 
     countdown = setInterval(function () {
         if (seconds > 0) {
