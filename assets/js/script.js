bb651b2deea01219f67493a56ae3fdcc90ce8193
Sam Wallace <sam@sammymayhem.com>
2022-04-03 14:57:54 -0700

    Added functionality to JS to show question our of array and answers in buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var timerEl = document.getElementById("timer");
 var startBtn = document.getElementById("start-btn");
 var answerBox = document.getElementById("answer-box");
+var questionBox = document.getElementById("question-box");
 
+var start = false;
+var score = 0;
 
 const Questions = [{
     id: 0,
@@ -15,12 +18,20 @@ const Questions = [{
 
 startBtn.addEventListener("click", function () {
     seconds = 5;
+    start = true;
 
     var starterText = document.getElementById("starter-text");
-        if (starterText.style.display == "none") {
+        if (starterText.style.display === "none") {
             starterText.style.display = "block";
         } else {
             starterText.style.display = "none"
+        }
+
+    var questionBox = document.getElementById("question-box");
+        if (questionBox.style.display !== "none") {
+            questionBox.style.display = "block";
+        } else {
+            questionBox.style.display = "none"
         }
 
     var answerBox = document.getElementById("answer-box");
@@ -38,4 +49,27 @@ startBtn.addEventListener("click", function () {
             timerEl.textContent = "Timer: 0";
         }
     }, 1000);
+})
+
+startBtn.addEventListener("click", function iterate() {
+    var question = document.getElementById("question-box");
+
+    question.innerText = Questions[0].q;
+
+    var option1 = document.getElementById("option1");
+    var option2 = document.getElementById("option2");
+    var option3 = document.getElementById("option3");
+    var option4 = document.getElementById("option4");
+    
+    option1.textContent = Questions[0].a[0].text;
+    option2.textContent = Questions[0].a[1].text;
+    option3.textContent = Questions[0].a[2].text;
+    option4.textContent = Questions[0].a[3].text;
+
+    option1.value = Questions[0].a[0].isCorrect;
+    option2.value = Questions[0].a[1].isCorrect;
+    option3.value = Questions[0].a[2].isCorrect;
+    option4.value = Questions[0].a[3].isCorrect;
+
+    
 })
