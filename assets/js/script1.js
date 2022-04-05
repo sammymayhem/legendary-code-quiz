b579a1e10b523a102b3dbcf07a486a8f0f8cd9da
Sam Wallace <sam@sammymayhem.com>
2022-04-04 22:45:48 -0700

    Added questions 3-5 functionality. Updated CSS and HTML. Sample questions.

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,20 +1,23 @@
 var timerEl = document.getElementById("timer");
 var startBtn = document.getElementById("start-btn");
 var question1 = document.getElementById("question-box1");
-var question2 = document.getElementById("question-box2")
+var question2 = document.getElementById("question-box2");
+var question3 = document.getElementById("question-box3");
+var question4 = document.getElementById("question-box4");
+var question5 = document.getElementById("question-box5");
 var correctOrNot = document.getElementById("answer");
 
 
-var timeLeft = 20;
-
 // Countdown timer
 function countdown() {
+    var timeLeft = 5;
+
     var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
-            timerEl.textContent = timeLeft;
+            timerEl.textContent = "Time: " + timeLeft;
             timeLeft--;
         } else {
-            timerEl.textContent = timerEl;
+            timerEl.textContent = "Time: " + timerEl;
             clearInterval(timeInterval);
         }
     }, 1000);
@@ -45,6 +48,10 @@ startBtn.addEventListener("click", function () {
     countdown();
     hideStart();
     showQuestion1();
+    hideQuestion2();
+    hideQuestion3();
+    hideQuestion4();
+    hideQuestion5();
 })
 
 // Show first question
@@ -79,4 +86,94 @@ function showQuestion2() {
     hideBottomeMsg();
     document.getElementById("question-box2").style.display = "block";
     document.getElementById("answer-box2").style.display = "block";
+}
+
+// Hide second question
+function hideQuestion2() {
+    document.getElementById("question-box2").style.display = "none";
+    document.getElementById("answer-box2").style.display = "none";
+}
+
+// Question 2 answer and to Question 3
+question2.addEventListener("click", function(event) {
+    var button = event.target;
+    if (button.matches(".userChoice")) {
+        var choice = button.textContent
+        var answer = button.parentElement.getAttribute("data-answer");
+        if (choice === answer) {
+            hideQuestion2();
+        } else {
+            timeAway();
+            hideQuestion2();
+        }
+    } showQuestion3();
+});
+
+// Show third question
+function showQuestion3() {
+    hideBottomeMsg();
+    document.getElementById("question-box3").style.display = "block";
+    document.getElementById("answer-box3").style.display = "block";
+}
+
+// Hide third question
+function hideQuestion3() {
+    document.getElementById("question-box3").style.display = "none";
+    document.getElementById("answer-box3").style.display = "none";
+}
+
+// Question 3 answer and to Question 4
+question3.addEventListener("click", function(event) {
+    var button = event.target;
+    if (button.matches(".userChoice")) {
+        var choice = button.textContent
+        var answer = button.parentElement.getAttribute("data-answer");
+        if (choice === answer) {
+            hideQuestion3();
+        } else {
+            timeAway();
+            hideQuestion3();
+        }
+    } showQuestion4();
+});
+
+// Show fourth question
+function showQuestion4() {
+    hideBottomeMsg();
+    document.getElementById("question-box4").style.display = "block";
+    document.getElementById("answer-box4").style.display = "block";
+}
+
+// Hide fourth question
+function hideQuestion4() {
+    document.getElementById("question-box4").style.display = "none";
+    document.getElementById("answer-box4").style.display = "none";
+}
+
+// Question 4 answer and to Question 5
+question4.addEventListener("click", function(event) {
+    var button = event.target;
+    if (button.matches(".userChoice")) {
+        var choice = button.textContent
+        var answer = button.parentElement.getAttribute("data-answer");
+        if (choice === answer) {
+            hideQuestion4();
+        } else {
+            timeAway();
+            hideQuestion4();
+        }
+    } showQuestion5();
+});
+
+// Show fifth question
+function showQuestion5() {
+    hideBottomeMsg();
+    document.getElementById("question-box5").style.display = "block";
+    document.getElementById("answer-box5").style.display = "block";
+}
+
+// Hide fifth question
+function hideQuestion5() {
+    document.getElementById("question-box5").style.display = "none";
+    document.getElementById("answer-box5").style.display = "none";
 }
